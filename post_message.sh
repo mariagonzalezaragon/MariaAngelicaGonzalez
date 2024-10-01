0a9046b9f8a0e04f3ae93d6ba494f22340cdaf6e
MariaGonzalez <maria_angelica.gonzalez_alarcon@lcieducation.net>
2024-10-01 01:13:05 -0400

    first commit

---
+++ post_message.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Lang: Shell Script
+
+URL="http://3.15.199.10/api"
+SYSTEM_INFO=$(hostname)
+
+DATA=$(cat<<EOF
+{
+"message":"Maria Angelica Gonzalez",
+"systemInfo":"$SYSTEM_INFO"
+}
+EOF
+)
+
+curl -X POST -H "Content-Type: application/json" -d "$DATA" $URL
